8102b6f15c5a4ad956d461ffea45b7e61c3de7d5
Niklas Wilming <niklaswilming@fastmail.fm>
2017-10-12 10:59:21 +0200

    PEP8 formatting and removal of duplicate code

--- glaze2.py
+++ glaze2.py
@@ -1,15 +1,15 @@
 # IMPORT STUFF
 
-from scipy.io import loadmat
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
 import collections
-import pandas as pd
-from scipy.stats import norm
 import math
-from scipy import optimize as opt
+import matplotlib.pyplot as plt
+import numpy as np
 import os
+import pandas as pd
+
+from scipy import optimize as opt
+from scipy.io import loadmat
+from scipy.stats import norm
 
 # 1. LOAD MATLAB FILE AND EXTRACT RELEVANT DATA IN ARRAY OR DATAFRAME
 
@@ -20,7 +20,11 @@ def path_name(sub_code, session, phase, block):
     '''returns the correct path + file of the matlab file in a specific folder.
 
     Input: subjects code, session A/B or C, phase 1 or 2, block 1 - 7.'''
-    directory = os.path.join('/Users/kenohagena/Documents/Data/', "{}".format(sub_code), "{}".format(session), 'PH_' + "{}".format(phase) + 'PH_' + "{}".format(block))
+    directory = os.path.join('/Users/kenohagena/Documents/Data/',
+                             "{}".format(sub_code),
+                             "{}".format(session), 'PH_' +
+                             "{}".format(phase) + 'PH_'
+                             + "{}".format(block))
     for file in os.listdir(directory):
         if file.endswith(".mat"):
             return os.path.join(directory, file)
@@ -33,9 +37,19 @@ def former_path_name(sub_code, session, phase, block):
 
     Input: subjects code, session A/B or C, phase 1 or 2, block 1 - 7.'''
     if block == 1:
-        directory = os.path.join('/Users/kenohagena/Documents/Data/', "{}".format(sub_code), "{}".format(session), 'PH_' + "{}".format(phase) + 'PH_' + "{}".format(block))
+        directory = os.path.join('/Users/kenohagena/Documents/Data/',
+                                 "{}".format(
+                                     sub_code), "{}".format(session),
+                                 'PH_' + "{}".format(phase) +
+                                 'PH_' + "{}".format(block))
     else:
-        directory = os.path.join('/Users/kenohagena/Documents/Data/', sub_code, session, 'PH_' + "{}".format(phase) + 'PH_' + "{}".format(block - 1))
+        directory = os.path.join('/Users/kenohagena/Documents/Data/',
+                                 sub_code,
+                                 session,
+                                 'PH_' +
+                                 "{}".format(phase) +
+                                 'PH_' +
+                                 "{}".format(block - 1))
     for file in os.listdir(directory):
         if file.endswith(".mat"):
             return os.path.join(directory, file)
@@ -43,6 +57,17 @@ def former_path_name(sub_code, session, phase, block):
 # 1.2 load matlab file and create an NUMPY ARRAY
 
 # function to load all data from the matlab file
+
+
+def row2dict(item):
+    '''
+    Convert a single row of the log to a dictionary.
+    '''
+    return {'time': item[0, 0][0, 0],
+            'message': item[0, 1][0],
+            'value': item[0, 2].ravel()[0],
+            'block': item[0, 3][0, 0],
+            'phase': item[0, 4][0, 0]}
 
 
 def load_np_unfiltered(sub_code, session, phase, block, key="p"):
@@ -53,12 +78,7 @@ def load_np_unfiltered(sub_code, session, phase, block, key="p"):
     x = loadmat(input_file)
     log = x["p"]['out'][0][0][0, 0][0]
 
-    def foo(item):
-        return {'time': item[0, 0][0, 0], 'message': item[0, 1][0], 'value': item[0, 2].ravel()[0],
-                'block': item[0, 3][0, 0],
-                'phase': item[0, 4][0, 0]}
-
-    pl = [foo(log[i, 0]) for i in range(log.shape[0])]
+    pl = [row2dict(log[i, 0]) for i in range(log.shape[0])]
 
     return np.array(pl)
 
@@ -75,16 +95,12 @@ def load_np_filtered(sub_code, session, phase, block, key="p"):
     x = loadmat(input_file)
     log = x["p"]['out'][0][0][0, 0][0]
 
-    def foo(item):
-        return {'time': item[0, 0][0, 0], 'message': item[0, 1][0], 'value': item[0, 2].ravel()[0],
-                'block': item[0, 3][0, 0],
-                'phase': item[0, 4][0, 0]}
-
-    pl = [foo(log[i, 0]) for i in range(log.shape[0])]
+    pl = [row2dict(log[i, 0]) for i in range(log.shape[0])]
     da = np.array(pl)
     return da[fsh:ash]
 
-# function to decide automatically which of the two options above shall be applied
+# function to decide automatically which of the two options above shall be
+# applied
 
 
 def load_np(sub_code, session, phase, block, key="p"):
@@ -110,12 +126,7 @@ def load_pd_unfiltered(sub_code, session, phase, block, key="p"):
     x = loadmat(input_file)
     log = x["p"]['out'][0][0][0, 0][0]
 
-    def foo(item):
-        return {'time': item[0, 0][0, 0], 'message': item[0, 1][0], 'value': item[0, 2].ravel()[0],
-                'block': item[0, 3][0, 0],
-                'phase': item[0, 4][0, 0]}
-
-    pl = [foo(log[i, 0]) for i in range(log.shape[0])]
+    pl = [row2dict(log[i, 0]) for i in range(log.shape[0])]
     df = pd.DataFrame(pl)
     df.loc[:, 'message'] = df.message.astype('str')
     return df
@@ -133,12 +144,7 @@ def load_pd_filtered(sub_code, session, phase, block, key="p"):
     x = loadmat(input_file)
     log = x["p"]['out'][0][0][0, 0][0]
 
-    def foo(item):
-        return {'time': item[0, 0][0, 0], 'message': item[0, 1][0], 'value': item[0, 2].ravel()[0],
-                'block': item[0, 3][0, 0],
-                'phase': item[0, 4][0, 0]}
-
-    pl = [foo(log[i, 0]) for i in range(log.shape[0])]
+    pl = [row2dict(log[i, 0]) for i in range(log.shape[0])]
     df = pd.DataFrame(pl)
     df.loc[:, 'message'] = df.message.astype('str')
     return df[fsh:ash]
@@ -165,7 +171,10 @@ def p_loc(DataFrame):
     array contains value of all point locations.'''
     df = DataFrame
     df.message = df.message.astype(str)
-    return np.array(df.query('message=="GL_TRIAL_LOCATION"').value.astype(float))
+    return np.array(df.query('message=="GL_TRIAL_LOCATION"')
+                    .value
+                    .astype(float))
+
 
 def p_loc_ind(DataFrame):
     '''returns indices of point locations as an array.
@@ -173,19 +182,23 @@ def p_loc_ind(DataFrame):
     array contains index of all point locations.'''
     df = DataFrame
     df.message = df.message.astype(str)
-    index1 = np.array(df.query('message=="GL_TRIAL_LOCATION"').index.astype(float))
+    index1 = np.array(
+        df.query('message=="GL_TRIAL_LOCATION"').index.astype(float))
     firstindex = np.array(df[:1].index.astype(float))
-    return np.subtract(index1,firstindex)
+    return np.subtract(index1, firstindex)
 
 # 2.2 EXTRACT RELEVANT SUBJECT DATA AT DECISION TRIALS
 
 # extract all answers
+
+
 def answers(DataFrame):
     '''returns numpy array with all subject answers.
 
     0 for x and 1 for m.'''
     df = DataFrame
-    choice = np.array(df.query('message=="CHOICE_TRIAL_RESP"').value.astype(float))
+    choice = np.array(
+        df.query('message=="CHOICE_TRIAL_RESP"').value.astype(float))
     return choice
 
 # extract answer indexes
@@ -194,7 +207,8 @@ def answers(DataFrame):
 def answer_ind(DataFrame):
     '''returns numpy array with all indexes of answers.'''
     df = DataFrame
-    choice_ind = np.array(df.query('message=="CHOICE_TRIAL_RESP"').index.astype(float))
+    choice_ind = np.array(
+        df.query('message=="CHOICE_TRIAL_RESP"').index.astype(float))
     return choice_ind
 
 # extract rule response
@@ -205,7 +219,8 @@ def rule_resp(DataFrame):
 
     1 corresponds with vertical-left, 0 == vertical right.'''
     df = DataFrame
-    rresp = np.array(df.query('message=="CHOICE_TRIAL_RULE_RESP"').value.astype(float))
+    rresp = np.array(
+        df.query('message=="CHOICE_TRIAL_RULE_RESP"').value.astype(float))
     return rresp
 
 
@@ -215,7 +230,8 @@ def stim(DataFrame):
 
     1 for horiyontal, 0 for vertical.'''
     df = DataFrame
-    stimulus = np.array(df.query('message=="GL_TRIAL_STIM_ID"').value.astype(float))
+    stimulus = np.array(
+        df.query('message=="GL_TRIAL_STIM_ID"').value.astype(float))
     return stimulus
 
 
@@ -227,8 +243,10 @@ def dist_source(DataFrame):
     df = DataFrame
     da = np.array(df)
     # first all indexes of 'generation side'
-    dist_ind = np.array(df.query('message=="GL_TRIAL_GENSIDE"').index.astype(float))
-    # mask to only contain 'gen side' in decision trial, thus only if 'stim_id' follows
+    dist_ind = np.array(
+        df.query('message=="GL_TRIAL_GENSIDE"').index.astype(float))
+    # mask to only contain 'gen side' in decision trial, thus only if
+    # 'stim_id' follows
     mask = []
     for n in dist_ind.astype(int):
         if da[(n - 1), 1] == "GL_TRIAL_STIM_ID":
@@ -242,7 +260,8 @@ def dist_source(DataFrame):
 def reward(DataFrame):
     '''returns array with all rewards.'''
     df = DataFrame
-    rewards = np.array(df.query('message=="GL_TRIAL_REWARD"').value.astype(float))
+    rewards = np.array(
+        df.query('message=="GL_TRIAL_REWARD"').value.astype(float))
     return rewards
 
 
@@ -252,8 +271,9 @@ def reward(DataFrame):
 def mod_choice(DataFrame, H):
     '''calculates belief of model over all trials.
 
-    returns choices model would have made before decision trial. Hazard rate optional argument.'''
-    return filter_dec(belief(DataFrame, H),DataFrame)
+    returns choices model would have made before decision trial. 
+    Hazard rate optional argument.'''
+    return filter_dec(belief(DataFrame, H), DataFrame)
 
 
 # Log likelihood ratio
@@ -261,19 +281,25 @@ def LLR(value, e1=0.5, e2=-0.5, sigma=1):
     '''returns log likelihood ratio.
 
     Needs means of both distributions and variance and a given value.'''
-    LLR = np.log(norm.pdf(value, e1, sigma)) - np.log(norm.pdf(value, e2, sigma))
+    LLR = np.log(norm.pdf(value, e1, sigma)) - \
+        np.log(norm.pdf(value, e2, sigma))
     return LLR
 
 # time varying prior expectation psi
+
+
 def prior(b_prior, H):
     '''returns weighted prior belief.
 
     given belief at t = n-1 and hazard rate.'''
-    psi = b_prior + np.log((1 - H) / H + np.exp(-b_prior)) - np.log((1 - H) / H + np.exp(b_prior))
+    psi = b_prior + np.log((1 - H) / H + np.exp(-b_prior)) - \
+        np.log((1 - H) / H + np.exp(b_prior))
     return psi
 
 # belief (as in Glaze et al.)
-def belief(DataFrame, H, e1 = 0.5, e2 = -0.5, sigma = 1):
+
+
+def belief(DataFrame, H, e1=0.5, e2=-0.5, sigma=1):
     '''Returns models Belief at a given time.
 
     Needs p_loc(Dataframe), means, variance and hazard rate.'''
@@ -287,12 +313,17 @@ def belief(DataFrame, H, e1 = 0.5, e2 = -0.5, sigma = 1):
             result.append(belief)
     return np.array(result)
 
-# function to filter out beliefs at relevant postitions, i.e. before decision trials
-def filter_dec(x,DataFrame):
+# function to filter out beliefs at relevant postitions, i.e. before
+# decision trials
+
+
+def filter_dec(x, DataFrame):
     '''filters only relevant values of belief function (above).
 
-    Returns belief values at those timepoints, when a decision trial follows.'''
-    # mask contains indices of p_loc in ALL TRIALS of p_locs before decision trial (thus only if 'stim_id' follows 4 logs later)
+    Returns belief values at those timepoints, when a decision trial follows.
+    '''
+    # mask contains indices of p_loc in ALL TRIALS of p_locs before decision
+    # trial (thus only if 'stim_id' follows 4 logs later)
     loc_indices = p_loc_ind(DataFrame)
     df = DataFrame
     da = np.array(df)
@@ -309,22 +340,31 @@ def filter_dec(x,DataFrame):
     for i in mask:
         maskc.append(np.where(loc_indices == i))
     return np.ravel(x[maskc])
-    #return maskc
+    # return maskc
 
 # 3.2 MODELS SUCCESS
 
 # first mapping model choice to rule response of subject
-def map_rresp(x):
-    '''function maps input to rule response of subject.
 
-    rule response of subject is mapped as follows: 1 == vertical-left, 0 == vertical righ.'''
-    return -(x>0).astype(float) + 1
+
+def map_rresp(x):
+    '''
+    function maps input to rule response of subject.
+
+    rule response of subject is mapped as follows:
+        1 == vertical-left
+        0 == vertical righ.
+    '''
+    return -(x > 0).astype(float) + 1
+
 
 def mod_suc(DataFrame, H):
-    '''counts choices in which the model micmicks succesfully the subjects answers.
+    '''
+    counts choices in which the model micmicks succesfully
+    the subjects answers.
 
     returns the count as a fraction of all choices made.'''
-    model = mod_choice(DataFrame,H)
+    model = mod_choice(DataFrame, H)
     sub = rule_resp(DataFrame)
     difference = map_rresp(model) - sub
     return collections.Counter(difference)[0.0] / len(difference)
@@ -338,6 +378,8 @@ def log_reg(x):
 
 # cross-entropy error function
 # rresp 0 == verticl right, 1==vertical left
+
+
 def ce_error_array(DataFrame, H):
     '''returns an array with single cross-entropy errors.'''
     pn = -rule_resp(DataFrame) + 1
@@ -345,18 +387,24 @@ def ce_error_array(DataFrame, H):
     return((1 - pn) * np.log(1 - pnm)) + (pn * np.log(pnm))
 
 # filters trials with 'nan' as result
+
+
 def filter_nan(x):
     '''filters only results that are not nan.'''
     result = []
     for i in x:
-        if math.isnan(i) == False:
+        if math.isnan(i) is False:
             result.append(i)
         else:
             continue
     return result
 
+
 def ce_error(DataFrame, H):
-    '''returns the cross=entropy error for a given input DataFrame dependent on the hazard rate H.'''
+    '''
+    returns the cross=entropy error for a given input 
+    DataFrame dependent on the hazard rate H.
+    '''
 
     return -sum(filter_nan(ce_error_array(DataFrame, H)))
 
@@ -380,9 +428,9 @@ def optimal_H(DataFrame):
     '''returns hazard rate with best cross entropy error.
 
     uses simple scalar optimization algorithm. time recquired: 50s.'''
-    def foo(x):
-        return ce_error(DataFrame, x)
-    o = opt.minimize_scalar(foo, bounds=(0, 1), method='bounded')
+
+    o = opt.minimize_scalar(lambda x: ce_error(DataFrame, x),
+                            bounds=(0, 1), method='bounded')
     return o
 
 
@@ -410,15 +458,16 @@ def plot_ce_H(DataFrame, x):
 
 
 __version__ = '2.1.2'
-#2.1.0
-### Version sperated loading functions from calculating functions and extractung functions.
-### Now matlab file has to be loaded and converted to dataframe in a first step.
-### All other functions take this dataframe as an input.
-### Thus, functions dont have to access matlab file and are supposedly faster
-#2.1.1
-###minor fix to filter_dec function
-#2.2.2
-### fix to model_choice function
-### H now obligatory parameter for model_choice
-
-
+'''
+2.1.0
+Version sperated loading functions from calculating functions and extracting
+functions.
+Now matlab file has to be loaded and converted to dataframe in a first step.
+All other functions take this dataframe as an input.
+Thus, functions dont have to access matlab file and are supposedly faster
+2.1.1
+minor fix to filter_dec function
+2.2.2
+fix to model_choice function
+H now obligatory parameter for model_choice
+'''
