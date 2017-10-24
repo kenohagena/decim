db06708d52d807c1ad3c049f9312b991607aad65
Keno Hagena <kenohagena@Kenos-MacBook-Pro.local>
2017-10-24 18:08:14 +0200

    PEP-8 modifications

--- pointsimulation.py
+++ pointsimulation.py
@@ -12,9 +12,11 @@ import random
 
 
 def what_trial(i):
-    '''Determines subsequent trial type according to p(decision trial): 'dt'.
+    '''
+    Determines subsequent trial type according to p(decision trial): 'dt'.
 
-    decides what function to use.'''
+    decides what function to use.
+    '''
     dt = 1 / 35  # probability of decision trial dt
     y = random.random()
     if y < dt:
@@ -27,7 +29,9 @@ def what_trial(i):
 
 
 def decision(i):
-    '''returns object of type dict containing index, message and rule.'''
+    '''
+    returns object of type dict containing index, message and rule.
+    '''
     x = {'index': i, 'message': 'decision'}
     return x
 
@@ -35,26 +39,36 @@ def decision(i):
 
 
 def location(i):
-    '''Ddecides whether to keep current distribution mean mu or change it with probability 'dt'.
+    '''
+    determines whether to change current distribution mean mu
+    with probability 'dt'
 
-    returns object of type dict which contains message, location value and rule.'''
+    returns dict containing message, location value and rule.
+    '''
     H = 1 / 70
     y = random.random()
     if y < H:
         global mu
         mu = -mu
         location = random.gauss(mu, sigma=1)
-        x = {'value': location, 'index': i, 'rule': mu, 'message': 'GL_TRIAL_LOCATION'}
+        x = {'value': location, 'index': i,
+             'rule': mu, 'message': 'GL_TRIAL_LOCATION'}
     else:
         mu = mu
         location = random.gauss(mu, sigma=1)
-        x = {'value': location, 'index': i, 'rule': mu, 'message': 'GL_TRIAL_LOCATION'}
+        x = {'value': location, 'index': i,
+             'rule': mu, 'message': 'GL_TRIAL_LOCATION'}
     return x
 
 
 # create pandas DataFrame
 # initialize DataFrame
 def simulate(x):
+    '''
+    simulates a dataset with x trials.
+
+    uses the function 'what_trial'.
+    '''
     columns = ['index', 'message', 'rule', 'value']
     index = [0]
     df = pd.DataFrame(index=index, columns=columns)
@@ -70,9 +84,11 @@ def simulate(x):
 
 
 def add_belief(df, H):
-    '''takes dataframe, computes belief according to glaze model with Hazard rate H.
+    '''
+    input: dataframe and hazardrate H, computes belief according to glaze.
 
-    returns concatenated Dataframe df containing former df plus belief.'''
+    returns concatenated dataframe containing former df plus belief.
+    '''
     glazes = glaze.belief(df, H)
     glazesdf = pd.DataFrame(glazes, columns=['belief'])
     df = pd.concat([df, glazesdf], axis=1)
@@ -80,38 +96,59 @@ def add_belief(df, H):
 
 
 def fill_decbel(df):
-    '''fills the belief field in each decision row by taking the belief at the last location trial.'''
+    '''
+    fills the belief field in each decision row
+
+    uses the belief at the last location trial.
+    '''
     decision_indices = df.loc[df.message == 'decision'].index
-    df.loc[df.message == 'decision', 'belief'] = df.loc[decision_indices - 1, 'belief'].values
+    df.loc[df.message == 'decision', 'belief'] = \
+        df.loc[decision_indices - 1, 'belief'].values
     return df
 
 
 def fill_decrule(df):
-    '''fills the rule field in each decision row by taking the rule at the last location trial.'''
+    '''
+    fills the rule field in each decision row
+
+    uses the rule at the last location trial.
+    '''
     decision_indices = df.loc[df.message == 'decision'].index
-    df.loc[df.message == 'decision', 'rule'] = df.loc[decision_indices - 1, 'rule'].values
+    df.loc[df.message == 'decision', 'rule'] = \
+        df.loc[decision_indices - 1, 'rule'].values
     return df
 
 
 def add_correct(df):
-    '''adds column with boolean index of correctness of models guess.'''
+    '''
+    adds column with boolean index of correctness of models guess.
+    '''
     df['correct'] = df['rule'] * df['belief'] > 0
     return df
 
 
 def complete(df, H):
-    '''takes simulated raw dataframe and Hazardrate.
+    '''
+    takes simulated raw dataframe and Hazardrate.
 
-    returns complete df containing message, location, belief, rule and correctness off belief over all trials.'''
+    returns dataframe with message, location, belief,
+    rule and correctness of belief.
+    '''
     return add_correct(fill_decrule(fill_decbel(add_belief(df, H))))
 
 
 def cordec(df):
-    '''returns percentage of correct answers at decicion trials.'''
+    '''
+    returns percentage of correct answers at decicion trials.
+    '''
     return df.loc[df.message == 'decision', 'correct'].mean()
 
 
-__version__ = '1.1'
+__version__ = '1.1.1'
 
-'''1.1
-changed probability of decision trial to 1/35.'''
+'''
+1.1
+changed probability of decision trial to 1/35.
+1.1.1
+PEP-8 fixes
+'''
