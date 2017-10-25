7b85db51ea1ad9e2bd442f467408bf58df4dd954
Keno Hagena <kenohagena@Kenos-MBP.fritz.box>
2017-10-25 21:17:27 +0200

    added funcionality: ce error function, opt_h function; made true hazardrate tH optional parameter for simulate function

--- pointsimulation.py
+++ pointsimulation.py
@@ -5,24 +5,27 @@ import pandas as pd
 import glaze2 as glaze
 import matplotlib.pyplot as plt
 import random
+from scipy import optimize as opt
+from scipy.special import expit  # logistic function
+
 
 # SIMULATE POINT LOCATIONS AND DECISION POINTS
 
 # decision trial or location trial
 
 
-def what_trial(i):
+def what_trial(i, tH):
     '''
     Determines subsequent trial type according to p(decision trial): 'dt'.
 
-    decides what function to use.
+    tH is true hazardrate.
     '''
     dt = 1 / 35  # probability of decision trial dt
     y = random.random()
     if y < dt:
         x = decision(i)
     else:
-        x = location(i)
+        x = location(i, tH)
     return x
 
 # decision trial
@@ -38,16 +41,15 @@ def decision(i):
 # location trial:
 
 
-def location(i):
+def location(i, tH):
     '''
     determines whether to change current distribution mean mu
-    with probability 'dt'
+    with probability 'dt' and hazardrate tH
 
     returns dict containing message, location value and rule.
     '''
-    H = 1 / 70
     y = random.random()
-    if y < H:
+    if y < tH:
         global mu
         mu = -mu
         location = random.gauss(mu, sigma=1)
@@ -63,9 +65,9 @@ def location(i):
 
 # create pandas DataFrame
 # initialize DataFrame
-def simulate(x):
+def simulate(x, tH=1 / 70):
     '''
-    simulates a dataset with x trials.
+    simulates a dataset with x trials and true hazardrate tH.
 
     uses the function 'what_trial'.
     '''
@@ -77,7 +79,7 @@ def simulate(x):
     global mu
     mu = random.choice([0.5, -0.5])
     for i in range(x):
-        df = df.append(what_trial(i), ignore_index=True)
+        df = df.append(what_trial(i, tH), ignore_index=True)
     return df
 
 # FILL IN MISSING INFORMATION
@@ -144,11 +146,43 @@ def cordec(df):
     return df.loc[df.message == 'decision', 'correct'].mean()
 
 
-__version__ = '1.1.1'
+def cer(df, H):
+    '''
+    Compute cross entropy error
+
+    compares models belief at decision point with actual rule.
+    '''
+    com = complete(df, H)
+    actualrule = com.loc[com.message == 'decision', 'rule'] + 0.5
+    modelbelief = expit(com.loc[com.message == 'decision', 'belief'])
+    error = -np.sum(((1 - actualrule) * np.log(1 - modelbelief)) +
+                    (actualrule * np.log(modelbelief)))
+    return error
+
+
+def opt_h(df):
+    '''
+    returns hazard rate with best cross entropy error.
+
+    uses simple scalar optimization algorithm.
+    '''
+
+    def error_function(x): return cer(df, x)
+    o = opt.minimize_scalar(error_function,
+                            bounds=(0, 1), method='bounded')
+    return o
+
+
+__version__ = '1.2'
 
 '''
 1.1
 changed probability of decision trial to 1/35.
 1.1.1
 PEP-8 fixes
+1.2
+added function to compute cross entropy error
+added function to calculate optimal model hazardrate
+made actual generating hazardrate optional parameter in simulate
 '''
+
