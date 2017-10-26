1a7ec2497f1a9b2dd8391d452bba38e74da137c0
Keno Hagena <kenohagena@Kenos-MacBook-Pro.local>
2017-10-26 14:32:00 +0200

    readability

--- glaze2.py
+++ glaze2.py
@@ -13,15 +13,13 @@ from scipy.special import expit  # logistic function
 
 # 1. LOAD MATLAB FILE AND EXTRACT RELEVANT DATA IN ARRAY OR DATAFRAME
 
-# 1.1 Preparation: Get path/file-name from subjectcode, etc.
-
 
 def load_log(sub_code, session, phase, block, base_path):
-    '''
-    returns the correct path + file of the matlab file in a specific folder.
+    """
+    Concatenates path and file name and loads matlba file.
 
-    Input: subjects code, session A/B or C, phase 1 or 2, block 1 - 7.
-    '''
+    Recquires subject code, session, phase and block.
+    """
     directory = join(base_path,
                      "{}".format(sub_code),
                      "{}".format(session),
@@ -39,9 +37,9 @@ def load_log(sub_code, session, phase, block, base_path):
 
 
 def row2dict(item):
-    '''
+    """
     Convert a single row of the log to a dictionary.
-    '''
+    """
     return {'time': item[0, 0][0, 0],
             'message': item[0, 1][0],
             'value': item[0, 2].ravel()[0],
@@ -49,11 +47,13 @@ def row2dict(item):
             'block': item[0, 4][0, 0]}
 
 
-def log2pd(log, block, key="p"):
-    '''takes loaded matlab file and returns panda dataframe.
 
-    contains only logs of that block.
-    '''
+def log2pd(log, block, key="p"):
+    """
+    Takes loaded matlab log and returns panda dataframe.
+
+    Extracts only logs of the current block.
+    """
     log = log["p"]['out'][0][0][0, 0][0]
     pl = [row2dict(log[i, 0]) for i in range(log.shape[0])]
 
@@ -63,35 +63,35 @@ def log2pd(log, block, key="p"):
     return df.query('block==%i' % block)
 
 
-def LLR(value, e1=0.5, e2=-0.5, sigma=1):
-    '''
-    returns log likelihood ratio.
+#CALCULATIONS
 
-    Needs means of both distributions and variance and a given value.
-    '''
+
+def LLR(value, e1=0.5, e2=-0.5, sigma=1):
+    """
+    Computes log likelihood ratio.
+
+    Takes means of two distributions and the common variance sigma.
+    """
     LLR = np.log(norm.pdf(value, e1, sigma)) - \
         np.log(norm.pdf(value, e2, sigma))
     return LLR
 
 
 def prior(b_prior, H):
-    '''
-    returns weighted prior belief.
-
-    given belief at t = n-1 and hazard rate.
-    '''
+    """
+    Returns weighted prior belief given blief at time t-1 and hazard rate
+    """
     psi = b_prior + np.log((1 - H) / H + np.exp(-b_prior)) - \
         np.log((1 - H) / H + np.exp(b_prior))
     return psi
 
 
 def belief(df, H, e1=0.5, e2=-0.5, sigma=1):
-    '''
-    Returns models Belief at a given time.
+    """
+    Returns models belief at a given time.
 
-    loc is a pandas Series that indexes into the overall
-    log file
-    '''
+    Takes panda dataframe and a hazardrate.
+    """
     locs = (df.loc[df.message == "GL_TRIAL_LOCATION", 'value']
             .astype(float))
     belief = 0 * locs.values
@@ -104,9 +104,9 @@ def belief(df, H, e1=0.5, e2=-0.5, sigma=1):
 
 
 def cross_entropy_error(df, H):
-    '''
-    Compute cross entropy error
-    '''
+    """
+    Computes cross entropy error, given dataframe and hazardrate.
+    """
 
     choices = (df.loc[df.message == "CHOICE_TRIAL_RULE_RESP", 'value']
                .astype(float))
@@ -121,11 +121,11 @@ def cross_entropy_error(df, H):
 
 
 def optimal_H(df):
-    '''
-    returns hazard rate with best cross entropy error.
+    """
+    Returns hazardrate with lowest cross entropy error.
 
-    uses simple scalar optimization algorithm. time recquired: 50s.
-    '''
+    Takes dataframe and uses simple scalar optimization algorithm.
+    """
     point_locations = (df.loc[df.message == "GL_TRIAL_LOCATION", 'value']
                        .astype(float))
     choices = (df.loc[df.message == "CHOICE_TRIAL_RULE_RESP", 'value']
@@ -135,14 +135,14 @@ def optimal_H(df):
     # Find last point location before choice trial
     belief_indices = df.loc[choices.index - 12].index.values
 
-    def error_function(x): return cross_entropy_error(
-        df, x)
+    def error_function(x):
+        return cross_entropy_error(df, x)
     o = opt.minimize_scalar(error_function,
                             bounds=(0, 1), method='bounded')
     return o
 
 
-__version__ = '3.2'
+__version__ = '3.2.1'
 '''
 2.1.0
 Version sperated loading functions from calculating functions and extracting
@@ -161,5 +161,6 @@ fix in log2pd: deleted the operation astype(float) on values, because ValueError
 in cross-entropy-error: inserted the definitions of choices and belief_indices from optimal_H
 3.2
 deleted obsolete plotting section
+3.2.1
+changes for better readability
 '''
-
