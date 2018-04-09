e12db531e901c4ab0d619b1df94451a85c329827
Keno Hagena <kenohagena@gmail.com>
2018-04-09 15:58:45 +0200

    debugged

--- decim/stan_simulation_fit_fixed.py
+++ decim/stan_simulation_fit_fixed.py
@@ -16,26 +16,26 @@ models = {'vfix': 'stan_models/inv_glaze_b_fixV.stan', 'gvfix':'stan_models/inv_
 def fix_keys():
     for V in [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]:
         for i in range(20):
-            yield(.015, V, 1, i, 'V', models['gvfix'], 'gvfix')
+            yield(.015, V, 1, i, 'V', models['gvfix'], 'gvfix', ['H', 'V'])
 
     for H in [.01, .05, .1, .15, .2, .25, .3, .35, .4, .45]:
-        for model in models:
+        for model, parameters in zip(['gvfix', 'vfix'], [['H', 'V'], ['H', 'gen_var']]):
             for i in range(20):
-                yield(H, 1, 1, i, 'H', models[model], model)
+                yield(H, 1, 1, i, 'H', models[model], model, parameters)
 
     for gen_var in [1, 1.5, 2, 2.5, 3, 3.5]:
         for i in range(20):
-            yield(.015, 1, gen_var, i, 'gen_var', models['vfix'], 'vfix')
+            yield(.015, 1, gen_var, i, 'gen_var', models['vfix'], 'vfix', ['H', 'gen_var'])
 
-def execute(H, V, gv, i, var, model, fixed_variable):
+def execute(H, V, gv, i, var, model, fixed_variable, parameters):
     model_file = decim.get_data(model)
     sm = pystan.StanModel(file=model_file)
     points = pt.fast_sim(trials, isi=isi)
     data = pt.complete(points, V=V, gen_var=gv, H=H, method='inverse')
     data = gs.data_from_df(data)
 
-    fit = sm.sampling(data=data, iter=100, chains=1)
+    fit = sm.sampling(data=data, iter=5000, chains=2)
     name = '{5}_{4}{3}_V={0}H={1}gv={2}'.format(V, H, gv, i, var, fixed_variable)
-    fitted = pw.fit_result(fit, parameters=['H', 'V', 'gen_var'])
+    fitted = pw.fit_result(fit, parameters=parameters)
     fitted.samples()
     fitted.to_csv(name=name, samples=True)
