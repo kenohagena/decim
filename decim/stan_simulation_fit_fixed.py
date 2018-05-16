3fa87b07f8d01aa858a2e13d0cf5482c3206ba2f
Keno Hagena <kenohagena@gmail.com>
2018-05-16 18:47:39 +0200

    min fix

--- decim/stan_simulation_fit_fixed.py
+++ decim/stan_simulation_fit_fixed.py
@@ -32,12 +32,12 @@ def fix_keys():
         trials = np.round(60 * 70 / (0.4 + 2. / isi)).astype(int)
         for V in [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]:
             for H in Hs:
-                for i in range(1):
+                for i in range(10):
                     yield(H, V, 1, i, 'V', models['gvfix'], 'gvfix', ['H', 'V'], isi, trials)
 
         for gen_var in [1, 1.5, 2, 2.5, 3, 3.5]:
             for H in Hs:
-                for i in range(1):
+                for i in range(10):
                     yield(H, 1, gen_var, i, 'gen_var', models['vfix'], 'vfix', ['H', 'gen_var'], isi, trials)
 
 
@@ -91,6 +91,6 @@ def grouper(iterable, n, fillvalue=None):
 
 def submit():
     from decim import slurm_submit as slu
-    for ii, chunk in enumerate(grouper(fix_keys(), 300)):
-        slu.pmap(par_execute, ii, chunk, walltime='11:30:00',
+    for ii, chunk in enumerate(grouper(fix_keys(), 2500)):
+        slu.pmap(par_execute, ii, chunk, walltime='11:55:00',
                  memory=40, nodes=1, tasks=16, name='PRECOVERY')
