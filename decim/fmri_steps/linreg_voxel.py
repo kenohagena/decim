79dd0d64273300e85c7718c9c30e030f79fe3ab7
Keno Hagena <kenohagena@gmail.com>
2018-08-06 16:56:18 +0200

    .hdf

--- decim/fmri_steps/linreg_voxel.py
+++ decim/fmri_steps/linreg_voxel.py
@@ -192,11 +192,10 @@ def hummel_ex(sub, ses):
     v.vol_2surf()
 
 
-def keys():
+def keys(ses):
     keys = []
     for sub in range(1, 23):
-        for ses in [2, 3]:
-            keys.append((sub, 'ses-{}'.format(ses)))
+        keys.append((sub, 'ses-{}'.format(ses)))
     return keys
 
 
@@ -206,8 +205,9 @@ def par_execute(keys):
 
 
 def submit():
-    slu.pmap(par_execute, keys(), walltime='2:55:00',
-             memory=60, nodes=1, tasks=12, name='fmri_align')
+    for ses in [2, 3]:
+        slu.pmap(par_execute, keys(ses), walltime='2:55:00',
+                 memory=60, nodes=1, tasks=8, name='fmri_align')
 
 
 #surface_data('/Volumes/flxrl/FLEXRULE/fmri/surface_textures', ['belief', 'rresp', 'response', 'LLR'])
