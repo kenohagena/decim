d9ff7d9b6792ebfeda0c7c7977389c0924ae8f5f
Keno Hagena <kenohagena@gmail.com>
2018-08-07 18:17:06 +0200

    parallelize for hummel

--- decim/fmri_steps/brainstem_coefs.py
+++ decim/fmri_steps/brainstem_coefs.py
@@ -24,37 +24,32 @@ atlases = [
 cit = pd.read_table(join(atlas_dir, 'CIT168_RL_Subcortical_Nuclei/CIT168_Reinf_Learn_v1/labels.txt'), sep='  ', header=None)
 
 
-def weighted_coef(subject, session, parameter):
-    nifti = nib.load(join(vox, '{0}_{1}_{2}.nii.gz'.format(subject, session, parameter)))
-    for a in atlases:
-        atlas = nib.load(join(atlas_dir, '{0}/{1}_T1w_{0}.nii.gz'.format(subject, a)))
-        atlas = resample_img(atlas, nifti.affine, target_shape=nifti.shape[0:3])
-        if a == 'CIT168_MNI':
-            for i in range(16):
-                atlasdata = atlas.get_data()[:, :, :, i] / atlas.get_data()[:, :, :, i].sum()
-                coef_ = np.multiply(nifti.get_data()[:, :, :, 0], atlasdata[:, :, :]).sum()
-                l_coef_.append({'subject': subject,
-                                'session': session,
-                                'atlas': cit.iloc[i, 1].replace(' ', ''),
-                                'parameter': parameter,
-                                'coef_': coef_})
-        else:
-            atlasdata = atlas.get_data() / atlas.get_data().sum()
-            coef_ = np.multiply(nifti.get_data()[:, :, :, 0], atlasdata[:, :, :, 0]).sum()
-            l_coef_.append({'subject': subject,
-                            'session': session,
-                            'atlas': a,
-                            'parameter': parameter,
-                            'coef_': coef_})
-
-
 def execute(sub, session):
     subject = 'sub-{}'.format(sub)
     l_coef_ = []
     for parameter in parameters:
         try:
-            weighted_coef(subject, session, parameter)
-            print(subject, session, parameter, 'done')
+            nifti = nib.load(join(vox, '{0}_{1}_{2}.nii.gz'.format(subject, session, parameter)))
+            for a in atlases:
+                atlas = nib.load(join(atlas_dir, '{0}/{1}_T1w_{0}.nii.gz'.format(subject, a)))
+                atlas = resample_img(atlas, nifti.affine, target_shape=nifti.shape[0:3])
+                if a == 'CIT168_MNI':
+                    for i in range(16):
+                        atlasdata = atlas.get_data()[:, :, :, i] / atlas.get_data()[:, :, :, i].sum()
+                        coef_ = np.multiply(nifti.get_data()[:, :, :, 0], atlasdata[:, :, :]).sum()
+                        l_coef_.append({'subject': subject,
+                                        'session': session,
+                                        'atlas': cit.iloc[i, 1].replace(' ', ''),
+                                        'parameter': parameter,
+                                        'coef_': coef_})
+                else:
+                    atlasdata = atlas.get_data() / atlas.get_data().sum()
+                    coef_ = np.multiply(nifti.get_data()[:, :, :, 0], atlasdata[:, :, :, 0]).sum()
+                    l_coef_.append({'subject': subject,
+                                    'session': session,
+                                    'atlas': a,
+                                    'parameter': parameter,
+                                    'coef_': coef_})
         except FileNotFoundError:
             print(subject, session, parameter)
     pd.DataFrame(l_coef_).to_hdf(join(vox, 'brainstem_coefs.hdf'), key='{0}_{1}'.format(subject, session))
