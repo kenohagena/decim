f12f2476aeadad8a431bf0645f2e4c3399a5db82
Keno Hagena <kenohagena@gmail.com>
2018-08-13 13:15:05 +0200

    for new workflow

--- decim/fmri_steps/warp_masks_MNI_to_T1w_subject_space.sh
+++ decim/fmri_steps/warp_masks_MNI_to_T1w_subject_space.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-atlas_dir=~/Flexrule/fmri/atlases
+subject=$1
+atlas_dir=$2
 cd "$atlas_dir"
-for subject in {1..22}; do
-    subdir=$atlas_dir/sub-$subject
-    echo $subdir
-    if [ ! -d "$subdir" ]; then
-        mkdir "$subdir"
-        for maskfile in $atlas_dir/selected/*.nii.gz; do
-            outfile=$atlas_dir/sub-$subject/${maskfile:$((${#atlas_dir}+10)):$((${#maskfile}-${#atlas_dir}-17))}_T1w_sub-$subject.nii.gz
-            warpfile=/Volumes/flxrl/fmri/completed_preprocessed/sub-"$subject"/fmriprep/sub-"$subject"/anat/sub-"$subject"_T1w_space-MNI152NLin2009cAsym_target-T1w_warp.h5
-            reference_file=/Volumes/flxrl/fmri/completed_preprocessed/sub-"$subject"/fmriprep/sub-"$subject"/anat/sub-"$subject"_T1w_preproc.nii.gz
-            antsApplyTransforms -d 3 -e 3 -i $maskfile -r $reference_file -o $outfile -n NearestNeighbor -t $warpfile -t identity -v
-        done
-    else
-        echo "Subject $subject already exists"
-    fi
-done
+subdir=$atlas_dir/$subject
+echo $subdir
+if [ ! -d "$subdir" ]; then
+    mkdir "$subdir"
+    for maskfile in $atlas_dir/selected/*.nii.gz; do
+        outfile=$atlas_dir/$subject/${maskfile:$((${#atlas_dir}+10)):$((${#maskfile}-${#atlas_dir}-17))}_T1w_$subject.nii.gz
+        warpfile=/Volumes/flxrl/FLEXRULE/fmri/completed_preprocessed/"$subject"/fmriprep/"$subject"/anat/"$subject"_T1w_space-MNI152NLin2009cAsym_target-T1w_warp.h5
+        reference_file=/Volumes/flxrl/FLEXRULE/fmri/completed_preprocessed/"$subject"/fmriprep/"$subject"/anat/"$subject"_T1w_preproc.nii.gz
+        antsApplyTransforms -d 3 -e 3 -i $maskfile -r $reference_file -o $outfile -n NearestNeighbor -t $warpfile -t identity -v
+    done
+else
+    echo "$subject already exists"
+fi
 find . -empty -type d -delete
