6ae681be64e2f7fb6e31a9fe92734e3407d1e785
Keno Hagena <kenohagena@gmail.com>
2018-10-18 16:22:33 +0200

    sss

--- decim/fmri_workflow/SubjectLevel.py
+++ decim/fmri_workflow/SubjectLevel.py
@@ -250,11 +250,21 @@ def execute(sub, ses, environment):
     for run in k:
         sl.BehavAligned['ses-{}'.format(ses)][run[run.find('in'):]] = pd.read_hdf(file, key=run)
     '''
+
+
     sl.BehavFrames()
     sl.RoiExtract()
     sl.BehavAlign(fast=True)
+    sl.PupilFrame = defaultdict(dict)
+    file = glob(join(sl.flex_dir, 'pupil/linear_pupilframes', '*Frame_{0}_ses-{1}.hdf'.format(sl.sub, ses)))
+    if len(file) != 1:
+        print(len(file), ' pupil frames found...')
+    with pd.HDFStore(file[0]) as hdf:
+        k = hdf.keys()
+    for run in k:
+        sl.PupilFrame['ses-{}'.format(ses)][run[run.find('in'):]] = pd.read_hdf(file[0], key=run)
     sl.ChoiceEpochs()
-    sl.SwitchEpochs()
+    #sl.SwitchEpochs()
     del sl.PupilFrame
     sl.CleanEpochs()
     sl.LinregVoxel()
