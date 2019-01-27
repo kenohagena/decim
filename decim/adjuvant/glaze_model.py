2c14034550d8313a3433c19695005e50acb488c9
Keno Hagena <kenohagena@gmail.com>
2019-01-27 16:29:45 +0100

    Documentation

---
+++ decim/adjuvant/glaze_model.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pandas as pd
+from glob import glob
+from os.path import join
+from scipy.stats import norm
+
+'''
+This script contains two functions, which are assessed by other scripts.
+
+1. "load_logs_bids"
+    - loads behavioral data from the bids data directory
+    - returns pd.DataFrame and name of file
+
+2. "belief"
+    - takes pd.DataFrame with behavior, Hazard rate and generative variance
+    - computes and returns Glaze belief, LLR, psi and Murphy surprise
+'''
+
+
+def load_logs_bids(subject, session, base_path, run='inference'):
+    '''
+    Loads behavioral data for all runs per session, subject and task
+    and returns behavioral pd.DataFrames
+
+    - Arguments:
+        a) subject (e.g. 'sub-17')
+        b) session (e.g. 'ses-2')
+        c) directory of raw dataset in BIDS format
+        d) task ('inference' or 'instructed')
+
+    - Output: dictionary with runs as keys and loaded pd.DataFrames as values
+    '''
+    if session == 'ses-1':
+        modality = 'beh'
+    else:
+        modality = 'func'
+    directory = join(base_path,
+                     subject,
+                     session,
+                     modality)
+    if run == 'instructed':
+        files = sorted(glob(join(directory, '*{}*.csv'.format(run))))
+    else:
+        files = sorted(glob(join(directory, '*{}*.tsv'.format(run))))
+    if len(files) == 0:
+        raise RuntimeError(
+            'No log file found for this block: %s, %s' %
+            (subject, session))
+    if run == 'inference':
+        return {file[-26:-11]: pd.read_table(file) for file in files}
+    elif run == 'instructed':
+        return {file[-27:-11]: pd.read_csv(file) for file in files}
+
+
+def LLR(value, e1=0.5, e2=-0.5, sigma=1):
+    """
+    Computes LLR.
+
+    Takes means of two distributions and the common variance sigma.
+    """
+    LLR = np.log(norm.pdf(value, e1, sigma)) - \
+        np.log(norm.pdf(value, e2, sigma))
+    return LLR
+
+
+def prior(b_prior, H):
+    """
+    Returns weighted prior belief given blief at time t-1 and hazard rate H.
+    """
+    psi = b_prior + np.log((1 - H) / H + np.exp(-b_prior)) - \
+        np.log((1 - H) / H + np.exp(b_prior))
+    return psi
+
+
+def murphy_surprise(psi, llr):
+    '''
+    Calculates surprise measure according to Peter.
+    '''
+    surprise = - (psi * llr)
+    return surprise
+
+
+def belief(df, H, gen_var=1, point_message='GL_TRIAL_LOCATION', ident='message'):
+    """
+    Computes Glaze belief, LLR, psi and Murphy surprise.
+
+    - Arguments:
+        a) pd.DataFrame with behavior (Output of load_logs_bids)
+        b) subjective hazard rate H of subject
+        c) generative variance in the Glaze model
+        d) message value in the behavioral pd.DataFrame that marks new point
+        e) column name of message values in behavioral pd.DataFrame
+
+    -Output: Glaze belief, LLR, psi, surprise all as pd.Series
+
+    """
+    locs = (df.loc[df['{}'.format(ident)] == point_message, 'value']
+            .astype(float))
+    belief = 0 * locs.values
+    psi = 0 * locs.values
+    for i, value in enumerate(locs):
+        if i == 0:
+            belief[i] = LLR(value, sigma=gen_var)
+        else:
+            belief[i] = prior(belief[i - 1], H) + LLR(value, sigma=gen_var)
+            psi[i] = prior(belief[i - 1], H)
+    surprise = murphy_surprise(psi, LLR(locs.values))
+    return pd.Series(belief, index=locs.index), pd.Series(psi, index=locs.index), pd.Series(LLR(locs.values), index=locs.index), pd.Series(surprise, index=locs.index)
+
+
+__version__ = '4.0.1'
+'''
+2.1.0
+Version sperated loading functions from calculating functions and extracting
+functions.
+Now matlab file has to be loaded and converted to dataframe in a first step.
+All other functions take this dataframe as an input.
+Thus, functions dont have to access matlab file and are supposedly faster
+2.1.1
+minor fix to filter_dec function
+2.1.2
+fix to model_choice function
+H now obligatory parameter for model_choice
+3.0 shortening by N
+3.1
+fix in log2pd: deleted the operation astype(float) on values, because ValueError was raised on subjects answers ('x' or 'm')
+in cross-entropy-error: inserted the definitions of choices and belief_indices from optimal_H
+3.2
+deleted obsolete plotting section
+3.2.1
+changes for better readability
+3.3
+performance function
+3.3.1
+mean RT
+3.3.2
+-accumulated evidence: now contains decision values in order to calculate choice consistency
+-accumulated evidence: contains 'objective evidence', i.e. evidence of model when given true H
+4.0.1
+-reduced to basic functionality
+'''
---
+++ decim/adjuvant/slurm_submit.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import subprocess
+import tempfile
+import os
+import errno
+
+
+'''
+Parallelize functions with simple arguments via SLURM
+'''
+
+
+def submit(walltime, memory, tmpdir, logdir, workdir, script, name,
+           nodes=1, tasks=16,
+           shellfname=None):
+    '''
+    Submit a script to SLURM
+    '''
+    print('script in submit {}'.format(script))
+    sbatch_directives = '''#!/bin/bash
+#SBATCH --job-name={name}
+#SBATCH --nodes={nodes}
+#SBATCH --tasks-per-node={tasks}
+#SBATCH --time={walltime}
+#SBATCH --export=NONE
+#SBATCH --mem={memory}GB
+#SBATCH --partition=std
+#SBATCH --mail-user=kenohagena@gmail.com
+#SBATCH --mail-type=NONE
+#SBATCH --error=/work/faty014/cluster/slurm_%j.out
+#SBATCH --output=/work/faty014/cluster/slurm_%j.err
+
+source /sw/modules/rrz-modules.sh
+
+
+    '''.format(**{'walltime': walltime,
+                  'nodes': nodes,
+                  'memory': memory,
+                  'tasks': tasks,
+                  'name': name,
+                  'logdir': logdir})
+
+    environment_variables = '''
+module purge
+module load env
+module load site/hummel
+source ~/.bashrc
+
+python3 {script}
+
+cp -r {workdir} /work/faty014
+    '''.format(**{'script': script,
+                  'workdir': workdir})
+    command = sbatch_directives + environment_variables
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, dir='/work/faty014/',
+                                     prefix='sbatch_script') as shellname:
+        shellname.write(command)
+        shellfname = shellname.name
+    command = "sbatch %s" % (shellfname)
+    output = subprocess.check_output(command,
+                                     stderr=subprocess.STDOUT, shell=True)
+    return output
+
+
+def to_script(func, tmpdir, *args):
+    '''
+    Write a simple stub python function that calls this function.
+    '''
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, dir='/work/faty014/',
+                                     prefix='py_script') as script:
+        code = """
+from {module} import {function}
+{function}{args}
+              """.format(**{'module': func.__module__,
+                            'function': func.__name__,
+                            'args': args})
+        script.write(code)
+    return str(script.name)
+
+
+def pmap(func, *args, walltime=12, memory=10,
+         tmp=None, name=None, tasks=16, env=None, nodes=1):
+    if name is None:
+        name = func.__name__
+    if tmp is None:
+        from os.path import join
+    tmp = tempfile.TemporaryDirectory().name
+    workdir = join(tmp, name)
+    tmpdir = join(workdir, 'tmp')
+    logdir = join(workdir, 'cluster_logs')
+    mkdir_p(tmpdir)
+    mkdir_p(logdir)
+    out = []
+    script = to_script(func, tmpdir, *args)
+    print('script in pmap = {}'.format(script))
+    pid = submit(walltime=walltime, memory=memory, logdir=logdir,
+                 tmpdir=tmpdir, script=script, name=name, nodes=nodes,
+                 tasks=tasks, workdir=workdir)
+    out.append(pid)
+    return out
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
