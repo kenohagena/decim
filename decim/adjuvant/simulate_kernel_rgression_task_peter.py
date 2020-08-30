dfef9fe42d53dadeff1f89f6b79aed88b1549612
Keno Hagena <kenohagena@gmail.com>
2020-08-30 15:02:29 +0200

    simulate and reg peters task

--- decim/adjuvant/simulate_kernel_rgression_task_peter.py
+++ decim/adjuvant/simulate_kernel_rgression_task_peter.py
@@ -56,35 +56,38 @@ def multi_trials(trials, H, V, var, dist_mean):
 
 def simulate_regression(trials, H, V, regression_C, n,
                         out_dir, var, dist_mean,
-                        sub='optimal_observer', regression_iter=1000):
-    simulated_trials = multi_trials(trials, H, V, var, dist_mean)
-    coefs = []
-    for i in range(regression_iter):
-        llr_cpp = simulated_trials.xs('pcp', axis=1, level=1, drop_level=True)
-        llr_cpp = (llr_cpp - llr_cpp.values.mean()) / llr_cpp.values.std()
-        llr_cpp = llr_cpp.multiply(simulated_trials.xs('LLR', axis=1, level=1, drop_level=True))
+                        sub='optimal_observer', regression_iter=1000, subs=20):
+    su = []
+    for s in range(subs):
+        simulated_trials = multi_trials(trials, H, V, var, dist_mean)
+        coefs = []
+        for i in range(regression_iter):
+            llr_cpp = simulated_trials.xs('pcp', axis=1, level=1, drop_level=True).drop(0, axis=1)
+            llr_cpp = (llr_cpp - llr_cpp.values.mean()) / llr_cpp.values.std()
+            llr_cpp = llr_cpp.multiply(simulated_trials.xs('LLR', axis=1, level=1, drop_level=True).drop(0, axis=1))
 
-        llr_psi = -simulated_trials.xs('psi', axis=1, level=1, drop_level=True).abs()
-        llr_psi = (llr_psi - llr_psi.values.mean()) / llr_psi.values.std()
-        llr_psi = llr_psi.multiply(simulated_trials.xs('LLR', axis=1, level=1, drop_level=True))
+            llr_psi = -simulated_trials.xs('psi', axis=1, level=1, drop_level=True).abs().drop(0, axis=1)
+            llr_psi = (llr_psi - llr_psi.values.mean()) / llr_psi.values.std()
+            llr_psi = llr_psi.multiply(simulated_trials.xs('LLR', axis=1, level=1, drop_level=True).drop(0, axis=1))
+            data = pd.concat([simulated_trials.xs('LLR', axis=1, level=1, drop_level=True), llr_cpp, llr_psi, simulated_trials.loc[:, 11].choice_prob], axis=1)
 
-        data = pd.concat([simulated_trials.xs('LLR', axis=1, level=1, drop_level=True), llr_cpp, llr_psi, simulated_trials.loc[:, 11].behavior.parameters.choice_probability], axis=1)
+            x = data.drop('choice_prob', axis=1)
+            x = (x - x.mean()) / x.std()
 
-        x = data.drop('choice_prob', axis=1)
-        x = (x - x.mean()) / x.std()
-        logreg = LogisticRegression(C=1)
-        logreg.fit(x.drop(0, axis=1).values, np.random.binomial(n=1, p=data.choice_prob))
-        coefs.append(logreg.coef_[0])
-    coefs = pd.DataFrame(coefs, columns=x.columns).mean()
-    coefs['n'] = n
-    coefs['trials'] = trials
-    coefs['H'] = H
-    coefs['V'] = V
-    coefs['C'] = regression_C
-    coefs['subject'] = sub
-    coefs['gen_var'] = var
-    coefs['dist_mean'] = dist_mean
-    coefs.to_hdf(join(out_dir, 'sim_reg_{0}_{1}_{2}_{3}.hdf'.format(var, H, sub, trials)), key=str(regression_C))
+            logreg = LogisticRegression(C=1)
+            logreg.fit(x.values, np.random.binomial(n=1, p=data.choice_prob))
+            coefs.append(logreg.coef_[0])
+        coefs = pd.DataFrame(coefs, columns=x.columns).mean()
+        coefs['n'] = n
+        coefs['trials'] = trials
+        coefs['H'] = H
+        coefs['V'] = V
+        coefs['C'] = regression_C
+        coefs['subject'] = sub
+        coefs['gen_var'] = var
+        coefs['dist_mean'] = dist_mean
+        su.append(coefs)
+    pd.DataFrame(su).to_hdf(join(out_dir, 'sim_reg_{0}_{1}_{2}_{3}.hdf'.format(var, H, sub, trials)), key=str(regression_C))
 
 
 def submit():
@@ -95,7 +98,7 @@ def submit():
         for gen_sigma in [1, 0.5, 0.75, 1.25]:
             for n in [12]:
                 for C in [1, 1e8]:
-                    pbs.pmap(simulate_regression, [(2500, H, 1, C, n, out_dir, gen_sigma)],
+                    pbs.pmap(simulate_regression, [(5000, H, 1, C, n, out_dir, gen_sigma)],
                              walltime='1:00:00', memory=15, nodes=1, tasks=1,
                              name='kernels')
 
